40489e06029fd55860a85cd4f615adb5c62d9b81
Vladislav Veluga <vladislav805@yandex.com>
2020-04-07 18:41:12 +0300

    Add Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import './style.scss';
+import classNames from 'classnames';
+
+type CheckboxOnSetChecked = (name: string, state: boolean) => void;
+
+export interface ICheckboxProps {
+    name: string;
+    value?: string;
+    label: string;
+    description?: string | { on: string; off: string };
+    checked?: boolean;
+    disabled?: boolean;
+    onSetChecked?: CheckboxOnSetChecked;
+}
+
+export interface ICheckboxState {
+    checked: boolean;
+}
+
+export default class Checkbox extends React.Component<ICheckboxProps, ICheckboxState> {
+    constructor(props: ICheckboxProps) {
+        super(props);
+
+        this.state = {
+            checked: props.checked,
+        };
+    }
+
+    private onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const checked = event.target.checked;
+        this.props.onSetChecked?.(this.props.name, checked);
+        this.setState({ checked });
+    };
+
+    private getSubLabel = () => {
+        const description = this.props.description;
+
+        if (typeof description === 'string') {
+            return description;
+        }
+
+        return this.state.checked
+            ? description.on
+            : description.off;
+    };
+
+    render() {
+        const { checked } = this.state;
+        const { name, value, label, description, disabled } = this.props;
+
+        return (
+            <label className={classNames('xCheckbox', {
+                'xCheckbox__checked': checked,
+                'xCheckbox__disabled': disabled,
+                'xCheckbox__has-description': description,
+            })}>
+                <input
+                    className="xCheckbox--native"
+                    type="checkbox"
+                    name={name}
+                    value={value}
+                    checked={checked}
+                    disabled={disabled}
+                    onChange={this.onChange} />
+                <div className="xCheckbox--shape" />
+                <div className="xCheckbox--content">
+                    <div className="xCheckbox--label">{label}</div>
+                    {description && (
+                        <div className="xCheckbox--description">{this.getSubLabel()}</div>
+                    )}
+                </div>
+            </label>
+        );
+    }
+}
