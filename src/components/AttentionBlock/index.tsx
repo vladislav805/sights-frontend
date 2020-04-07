54d21a04190d715c7bfe29837a25aaa5245fcd55
Vladislav Veluga <vladislav805@yandex.com>
2020-04-08 00:36:03 +0300

    AttentionBlock now apply in text `() => string` and `string`

--- src/components/AttentionBlock/index.tsx
+++ src/components/AttentionBlock/index.tsx
@@ -5,13 +5,13 @@ import classNames from 'classnames';
 interface IAttentionBlockProps {
     show?: boolean;
     type: 'error' | 'warning' | 'info';
-    text?: () => string;
+    text?: string | (() => string);
 }
 
-const noop = () => '';
-const AttentionBlock = ({ show = false, type = 'error', text = noop}: IAttentionBlockProps) => {
+const AttentionBlock = ({ show = false, type = 'error', text = ''}: IAttentionBlockProps) => {
+    const str = show ? (typeof text === 'function' ? text() : text) : undefined;
     return show
-        ? <div className={classNames('attention', `attention__${type}`)}>{text()}</div>
+        ? <div className={classNames('attention', `attention__${type}`)}>{str}</div>
         : null;
 };
 
