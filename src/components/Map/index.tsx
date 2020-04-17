8e5c1da3d79a72991f8e47dc351becba82fe3d8c
Vladislav Veluga <vladislav805@yandex.com>
2020-04-17 16:17:56 +0300

    Add gm+2g tiles, remove unused code

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import './leaflet.scss';
-import { Map, TileLayer, LayersControl, withLeaflet, ContextProps, Rectangle, Marker, Popup, Viewport, Tooltip } from 'react-leaflet';
+import { ContextProps, LayersControl, Map, TileLayer, withLeaflet } from 'react-leaflet';
 import { hostedLocalStorage } from '../../utils/localstorage';
-import { LatLngTuple } from 'leaflet';
 import * as Leaflet from 'leaflet';
+import { LatLngTuple } from 'leaflet';
 import { getCoordinatesFromMap } from './utils';
-import { ICity, ISight } from '../../api';
-import SightLayout from './SightLayout';
 
 interface IMapProps<T = unknown> {
     onMapReady?: (map: Leaflet.Map) => void;
     onLocationChanged?: (ne: LatLngTuple, sw: LatLngTuple) => void;
     saveLocation?: boolean;
-    debugRect?: boolean;
 
     items: T[];
     drawItem: (item: T) => React.ReactChild;
@@ -20,29 +17,63 @@ interface IMapProps<T = unknown> {
 
 interface IMapState {
     popupOpened: boolean;
-    __rect?: [LatLngTuple, LatLngTuple];
+//    __rect?: [LatLngTuple, LatLngTuple];
 }
 
 const defaultName = 'osm';
 const tiles = [
     {
         name: 'osm',
-        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
         title: 'OpenStreetMap',
+        subdomains: [],
         copyrights: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     },
     {
-        name: 'gms',
-        url: 'http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga',
-        title: 'Google Maps Schema',
+        name: 'gmsh',
+        url: 'http://mt{s}.google.com/vt/lyrs=s,h&hl=en&x={x}&y={y}&z={z}&s=Ga',
+        title: 'Google Maps Hybrid',
+        subdomains: ['0', '1', '2'],
         copyrights: '&copy; Google Maps',
     },
     {
-        name: 'gmt',
-        url: 'http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga',
+        name: 'gms',
+        url: 'http://mt{s}.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga',
         title: 'Google Maps Satellite',
+        subdomains: ['0', '1', '2'],
         copyrights: '&copy; Google Maps',
     },
+
+    {
+        name: 'gmm',
+        url: 'http://mt{s}.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga',
+        title: 'Google Maps Streets',
+        subdomains: ['0', '1', '2'],
+        copyrights: '&copy; Google Maps',
+    },
+    {
+        name: 'gmp',
+        url: 'http://mt{s}.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}&s=Ga',
+        title: 'Google Maps Terrarian',
+        subdomains: ['0', '1', '2'],
+        copyrights: '&copy; Google Maps',
+    },
+    {
+        name: '2gis',
+        url: 'http://tile{s}.maps.2gis.com/tiles?x={x}&y={y}&z={z}',
+        title: '2gis',
+        subdomains: ['0', '1', '2'],
+        copyrights: '2gis'
+    }
+    /*{
+        name: 'ym',
+        url: 'http://vec{s}.maps.yandex.net/tiles?l=map&v=4.55.2&z={z}&x={x}&y={y}&scale=2&lang=ru_RU',
+        copyrights: '<a href="https://yandex.ru/maps" target="_blank">Яндекс</a>',
+        subdomains: ['01', '02', '03', '04'],
+        options: {
+            crs: Leaflet.CRS.EPSG3395,
+        },
+    }*/
 ];
 
 const PREF_LAST_CENTER = 'last_center';
@@ -55,20 +86,24 @@ class MapX extends React.Component<IMapProps & ContextProps, IMapState> {
 
     state: IMapState = {
         popupOpened: false,
-        __rect: null,
+//        __rect: null,
     };
+
+    private getMap = (): Leaflet.Map => this.mapRef?.current?.leafletElement;
 
     private prefs = hostedLocalStorage('map');
     private mapRef = React.createRef<Map>();
     private last = { lat: 0, lng: 0 };
 
     componentDidMount() {
-        this.props.onMapReady?.(this.mapRef?.current?.leafletElement);
+        this.props.onMapReady?.(this.getMap());
+
+        console.log(this.mapRef.current?.leafletElement.options);
     }
 
-    private onViewportChanged = (event: Leaflet.LeafletEvent) => {
+    private onViewportChanged = () => { // event: Leaflet.LeafletEvent
     //private onViewportChanged = (viewport: Viewport) => {
-        const map = this.mapRef.current?.leafletElement;
+        const map = this.getMap();
 
         const { lat, lng } = map.getCenter();
 
@@ -88,9 +123,6 @@ class MapX extends React.Component<IMapProps & ContextProps, IMapState> {
         }
     }
 
-    private onPopupOpen = () => this.setState({ popupOpened: true });
-    private onPopupClose = () => this.setState({ popupOpened: false });
-
     render() {
         const p = this.prefs;
         const center = p(PREF_LAST_CENTER)?.split(',').map(Number) as LatLngTuple ?? [60, 30];
@@ -102,19 +134,21 @@ class MapX extends React.Component<IMapProps & ContextProps, IMapState> {
                 zoom={+p(PREF_LAST_ZOOM) || 10}
                 onmoveend={this.onViewportChanged}>
                 <LayersControl position="topright">
-                    {tiles.map(({ name, title, url, copyrights }) => (
-                        <LayersControl.BaseLayer key={title} name={title} checked={name === defaultName}>
+                    {tiles.map(({ name, title, url, copyrights, subdomains }) => (
+                        <LayersControl.BaseLayer
+                            key={title}
+                            name={title}
+                            checked={name === defaultName}>
                             <TileLayer
                                 attribution={copyrights}
-                                url={url} />
+                                url={url}
+                                subdomains={subdomains} />
+
                         </LayersControl.BaseLayer>
                     ))}
                 </LayersControl>
-                {this.props.children}
                 {this.props.items.map(this.props.drawItem)}
-                {this.props.debugRect && this.state.__rect && (
-                    <Rectangle bounds={this.state.__rect} fillColor="red" fillOpacity={.5} />
-                )}
+                { /* this.props.debugRect && this.state.__rect && (<Rectangle bounds={this.state.__rect} fillColor="red" fillOpacity={.5} />) */ }
             </Map>
         );
     }
