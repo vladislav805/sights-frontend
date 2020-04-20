703e8d21c71bfb1dd8d3b6d2b84ced59f956882c
Vladislav Veluga <vladislav805@yandex.com>
2020-04-21 02:54:41 +0300

    Using genderize

--- src/pages/User/lastSeen.ts
+++ src/pages/User/lastSeen.ts
@@ -1,14 +1,9 @@
-import { IUser, UserSex } from '../../api';
+import { IUser } from '../../api';
 import { humanizeDateTime, Format } from '../../utils';
+import { genderize } from '../../utils/genderize';
 
 const lastSeen = (unixtime: number) => humanizeDateTime(new Date(unixtime * 1000), Format.DATE | Format.TIME);
 
-const was: Record<UserSex, string> = {
-    [UserSex.FEMALE]: 'Заходила',
-    [UserSex.MALE]: 'Заходил',
-    [UserSex.NOT_SET]: 'Заходило',
-};
-
 export const getLastSeen = (user: IUser) => user.isOnline
     ? 'Сейчас здесь'
-    : `${was[user.sex]} ${lastSeen(user.lastSeen)}`;
+    : `${genderize(user, 'Заходил', 'Заходила')} ${lastSeen(user.lastSeen)}`;
