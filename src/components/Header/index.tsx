751533fad720964f3f8e1a7c71207fc7c8b5cf3d
Vladislav Veluga <vladislav805@yandex.com>
2020-04-21 13:31:46 +0300

    Some of dark theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,6 @@ import Logo from '../Logo';
 import { IUser } from '../../api';
 import { RootStore, setSession, TypeOfConnect } from '../../redux';
 import { Link } from 'react-router-dom';
-import LinkIcon from '../LinkIcon';
 import Icon from '@mdi/react';
 
 const storeEnhancer = connect(
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import './scss/reset.scss';
+import './scss/theme.scss';
 import './scss/common.scss';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
