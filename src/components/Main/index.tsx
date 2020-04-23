d20182e4b6f45fbc64f11fdd5ca46dd94471ae28
Vladislav Veluga <vladislav805@yandex.com>
2020-04-23 17:50:57 +0300

    Use React.Suspense for bundle splitting

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './style.scss';
 import { Switch, Route } from 'react-router-dom';
-import Home from '../../pages/Home';
-import User from '../../pages/User';
+const Home = React.lazy(() => import(/* webpackChunkName: 'page.home' */ '../../pages/Home'));
+const User = React.lazy(() => import(/* webpackChunkName: 'page.user' */'../../pages/User'));
 import Sight from '../../pages/Sight';
-import Island from '../../pages/Island';
-import Feed from '../../pages/Feed';
-import Page from '../../pages/Page';
+const Island = React.lazy(() => import(/* webpackChunkName: 'page.island' */'../../pages/Island'));
+const Feed = React.lazy(() => import(/* webpackChunkName: 'page.feed' */'../../pages/Feed'));
+const Page = React.lazy(() => import(/* webpackChunkName: 'page.pages' */'../../pages/Page'));
 import Menu from '../Menu';
+import LoadingWrapper from '../LoadingWrapper';
 
 type IMenuProps = {
     menu: boolean;
@@ -19,14 +20,16 @@ const Main = ({ menu, closeMenu }: IMenuProps) => (
         <div className="main-container">
             <Menu isOpen={menu} close={closeMenu} />
             <main>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/user/:username" component={User} />
-                    <Route path="/sight" component={Sight} />
-                    <Route path="/island" component={Island} />
-                    <Route path="/feed" component={Feed} />
-                    <Route path="/page/:id" component={Page} />
-                </Switch>
+                <React.Suspense fallback={<LoadingWrapper loading />}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/user/:username" component={User} />
+                        <Route path="/sight" component={Sight} />
+                        <Route path="/island" component={Island} />
+                        <Route path="/feed" component={Feed} />
+                        <Route path="/page/:id" component={Page} />
+                    </Switch>
+                </React.Suspense>
             </main>
         </div>
     </div>
--- src/pages/Sight/index.tsx
+++ src/pages/Sight/index.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import LoadingWrapper from '../../components/LoadingWrapper';
 import Random from './Random';
-import Search from './Search';
-import MapPage from './Map';
-import SightEntry from './Entry';
-import SightEdit from './Edit';
+const Search = React.lazy(() => import(/* webpackChunkName: 'page.sight.search' */ './Search'));
+const MapPage = React.lazy(() => import(/* webpackChunkName: 'page.sight.map' */ './Map'));
+const SightEntry = React.lazy(() => import(/* webpackChunkName: 'page.sight.entry' */ './Entry'));
+const SightEdit = React.lazy(() => import(/* webpackChunkName: 'page.sight.edit' */ './Edit'));
 
 const SightRoute = () => (
-    <Switch>
-        <Route path="/sight/random" component={Random} />
-        <Route path="/sight/search" component={Search} />
-        <Route path="/sight/map" component={MapPage} />
-        <Route path="/sight/new" component={SightEdit} />
-        <Route path="/sight/:id/edit" component={SightEdit} />
-        <Route path="/sight/:id" component={SightEntry} />
-    </Switch>
+    <React.Suspense fallback={<LoadingWrapper loading />}>
+        <Switch>
+            <Route path="/sight/random" exact component={Random} />
+            <Route path="/sight/search" exact component={Search} />
+            <Route path="/sight/map" exact component={MapPage} />
+            <Route path="/sight/new" exact component={SightEdit} />
+            <Route path="/sight/:id/edit" component={SightEdit} />
+            <Route path="/sight/:id" component={SightEntry} />
+        </Switch>
+    </React.Suspense>
 );
 
 export default SightRoute;
