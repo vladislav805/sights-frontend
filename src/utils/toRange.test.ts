6b20bfc63c26eca947b07e1dea1d705fff5c127a
Vladislav Veluga <vladislav805@yandex.com>
2020-04-25 14:50:35 +0300

    toRange tests

---
+++ src/utils/toRange.test.ts
@@ -0,0 +1,15 @@
+import { toRange } from '.';
+
+describe('toRange function', () => {
+    it('should return same number if in range', () => {
+        expect(toRange(5, 0, 10)).toBe(5);
+    });
+
+    it('should return min value if out of minimal range', () => {
+        expect(toRange(-5, 0, 10)).toBe(0);
+    });
+
+    it('should return max value if out of maximum range', () => {
+        expect(toRange(15, 0, 10)).toBe(10);
+    });
+});
