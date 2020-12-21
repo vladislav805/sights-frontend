a611fa0c282a06d921f20e728b109b857fb65a02
Vladislav Veluga <vladislav805@yandex.com>
2020-12-21 19:05:53 +0300

    Мелкие изменения в коде и типах

--- src/external-types.ts
+++ src/external-types.ts
@@ -1,8 +1,18 @@
 declare module 'react-leaflet-markercluster' {
-    // import { createPathComponent } from '@react-leaflet/core';
     // TODO update typings
     // https://github.com/YUzhva/react-leaflet-markercluster
     // https://github.com/yuzhva/react-leaflet-markercluster/blob/master/src/react-leaflet-markercluster.js
     // https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-leaflet-markercluster
+
+    /**
+     * function createPathComponent
+     * <E extends FeatureGroup | Path, P extends PathWithChildrenProps>
+ *     (
+     *     createElement: (props: P, context: LeafletContextInterface) => LeafletElement<E>,
+     *     updateElement?: (instance: E, props: P, prevProps: P) => void
+ *     ):
+     *     import("react").ForwardRefExoticComponent<import("react").PropsWithoutRef<P> & import("react").RefAttributes<E>>;
+     */
+
     export default class MarkerClusterGroup extends React.Component {}
 }
