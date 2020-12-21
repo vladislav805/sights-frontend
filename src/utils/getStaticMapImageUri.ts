ec5ba53d2679c793bc50eb2bf77ad0975f8bbc2a
Vladislav Veluga <vladislav805@yandex.com>
2020-12-21 19:06:43 +0300

    Добавление параметра x2 к статической карте

--- src/utils/getStaticMapImageUri.ts
+++ src/utils/getStaticMapImageUri.ts
@@ -4,8 +4,9 @@ type StaticMapProps = {
     lat: number;
     lng: number;
     zoom: number;
+    x2?: boolean;
 }
-export const getStaticMapImageUri = ({ width, height, lat, lng, zoom }: StaticMapProps): string => `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson(${JSON.stringify({
+export const getStaticMapImageUri = ({ width, height, lat, lng, zoom, x2 }: StaticMapProps): string => `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson(${JSON.stringify({
     type: 'Point',
     coordinates: [lng, lat]
-})})/${lng},${lat},${zoom},0/${width}x${height}?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`;
+})})/${lng},${lat},${zoom},0/${width}x${height}${x2 ? '@2x' : ''}?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`;
