6972bd1af298230156c2312c21589181effc0349
Vladislav Veluga <vladislav805@yandex.com>
2020-12-29 13:45:06 +0300

    MenuOverlay в React.FC

--- src/components/Menu/overlay.tsx
+++ src/components/Menu/overlay.tsx
@@ -2,45 +2,26 @@ import * as React from 'react';
 import { createPortal } from 'react-dom';
 import * as classNames from 'classnames';
 
-interface IMenuOverlayProps {
+type IMenuOverlayProps = {
     show: boolean;
     close: () => void;
 }
 
-class MenuOverlay extends React.Component<IMenuOverlayProps> {
-    private readonly overlay: HTMLDivElement;
+const MenuOverlay: React.FC<IMenuOverlayProps> = (props: IMenuOverlayProps) => {
+    React.useEffect(() => {
+        document.body.style.overflow = props.show ? 'hidden' : 'auto';
+    }, [props.show]);
 
-    constructor(props: IMenuOverlayProps) {
-        super(props);
-        this.overlay = document.createElement('div');
-    }
-
-    componentDidMount(): void {
-        document.body.appendChild(this.overlay);
-    }
-
-    componentDidUpdate(prevProps: IMenuOverlayProps): void {
-        if (prevProps.show !== this.props.show) {
-            document.body.style.overflow = this.props.show ? 'hidden' : 'auto';
-        }
-    }
-
-    componentWillUnmount(): void {
-        document.body.removeChild(this.overlay);
-    }
-
-    render(): JSX.Element {
-        return createPortal(
-            (
-                <div
-                    onClick={this.props.close}
-                    className={classNames('menu-overlay', {
-                        'menu-overlay__open': this.props.show,
-                    })} />
-            ),
-            this.overlay
-        );
-    }
+    return createPortal(
+        (
+            <div
+                onClick={props.close}
+                className={classNames('menu-overlay', {
+                    'menu-overlay__open': props.show,
+                })} />
+        ),
+        document.body,
+    );
 }
 
 export default MenuOverlay;
