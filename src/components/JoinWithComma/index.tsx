6582e158c830f962eb50321e48eb7aa1b446490d
Vladislav Veluga <vladislav805@yandex.com>
2021-01-27 04:09:21 +0300

    Оптимизация JoinWithComma

--- src/components/JoinWithComma/index.tsx
+++ src/components/JoinWithComma/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 
 type IJoinWithCommaProps = React.PropsWithChildren<unknown>;
 
-const JoinWithComma: React.FC<IJoinWithCommaProps> = (props: IJoinWithCommaProps) => (
+const JoinWithComma: React.FC<IJoinWithCommaProps> = (props: IJoinWithCommaProps) => React.useMemo(() => (
     <>
-        {React.Children.toArray(props.children).map((child, index) => index ? <>, {child}</> : child)}
+        {React.Children.toArray(props.children).map((child, index) => index
+            ? <React.Fragment key={index}>, {child}</React.Fragment>
+            : child)}
     </>
-);
+), [props.children]);
 
 export default JoinWithComma;
