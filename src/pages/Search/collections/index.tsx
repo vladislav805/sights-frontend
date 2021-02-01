3ee84e4e82e90db16286834fdbdcfd9b64ab9f20
Vladislav Veluga <vladislav805@yandex.com>
2021-02-01 21:22:36 +0300

    Поиск коллекций

--- src/pages/Search/collections/index.tsx
+++ src/pages/Search/collections/index.tsx
@@ -4,9 +4,15 @@ import StickyHeader from '../../../components/StickyHeader';
 import TextInput from '../../../components/TextInput';
 import Button from '../../../components/Button';
 import LoadingSpinner from '../../../components/LoadingSpinner';
-import API from '../../../api';
+import { apiExecute } from '../../../api';
 import CollectionGallery from '../../../components/CollectionGallery';
 import { ISearchEntryProps } from '../common';
+import { IApiList } from '../../../api/types/api';
+import { ICity } from '../../../api/types/city';
+import { ICollection } from '../../../api/types/collection';
+import FakeTextInput from '../../../components/FakeTextInput';
+import CityModal from '../../../components/CityModal';
+import * as Modal from '../../../components/Modal';
 
 type IParams = Partial<{
     query: string;
@@ -15,8 +21,13 @@ type IParams = Partial<{
 
 const PEER_PAGE = 20;
 
+type IResultSearch = {
+    search: IApiList<ICollection>;
+    city: ICity | null;
+};
+
 const fetchFactory = (params: IParams, offset: number) => () => {
-    return API.collections.search({
+    return apiExecute<IResultSearch>('const search=API.collections.search(A),city=A.cityId?API.cities.getById({cityIds:A.cityId})?.[0]:null;return{search,city};', {
         query: params.query,
         cityId: +params.cityId,
         fields: ['collection_city', 'collection_rating'],
@@ -29,8 +40,17 @@ export const SearchCollections: React.FC<ISearchEntryProps> = (props: ISearchEnt
     // Создание функции для запроса по URL
     const fetcher = React.useMemo(() => fetchFactory(props.params, props.offset), [props.params]);
 
+    const [city, setCity] = React.useState<ICity | null>(null);
+    const [showCityModal, setShowCityModal] = React.useState<boolean>(false);
+
     // Использование ответа от API
     const { result, loading } = useApiFetch(fetcher);
+
+    const { search, city: reqCity } = result ?? {};
+
+    React.useEffect(() => {
+        setCity(reqCity);
+    }, [reqCity]);
 
     // Объект с данными из текстовых полей формы
     const [formParams, setFormParams] = React.useState<IParams>(props.params);
@@ -61,6 +81,10 @@ export const SearchCollections: React.FC<ISearchEntryProps> = (props: ISearchEnt
                         name="query"
                         value={formParams.query}
                         onChange={onChangeText} />
+                    <FakeTextInput
+                        label="Город"
+                        value={city ? city.name : 'не выбран'}
+                        onClick={() => setShowCityModal(true)} />
                     <Button
                         type="submit"
                         label="Поиск"
@@ -70,14 +94,25 @@ export const SearchCollections: React.FC<ISearchEntryProps> = (props: ISearchEnt
             {loading && (
                 <LoadingSpinner block />
             )}
-            {!loading && result && (
+            {!loading && search && (
                 <CollectionGallery
-                    count={result.count}
-                    items={result.items}
+                    count={search.count}
+                    items={search.items}
                     peerPage={PEER_PAGE}
                     offset={props.offset}
                     onOffsetChange={props.onOffsetChange}/>
             )}
+            <Modal.Window
+                show={showCityModal}
+                onOverlayClick={() => setShowCityModal(false)}>
+                <CityModal
+                    selected={city}
+                    onChange={city => {
+                        setFormParams({ ...formParams, cityId: city ? String(city.cityId) : null });
+                        setCity(city);
+                        setShowCityModal(false);
+                    }} />
+            </Modal.Window>
         </div>
     );
 };
